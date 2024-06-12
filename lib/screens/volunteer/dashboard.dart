cf76ff96a73eba9764ff9869a81cea47dc4d454b
Jeanine96 <136748816+Jeanine96@users.noreply.github.com>
2024-06-12 16:47:34 +0200

    [build] Added title to dashboard

--- lib/screens/volunteer/dashboard.dart
+++ lib/screens/volunteer/dashboard.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiet_app/constants/constants.dart';
+import 'package:quiet_app/screens/volunteer/components/dashboard-tickets.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
@@ -10,6 +12,19 @@ class DashboardPage extends StatefulWidget {
 class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80),
+        child: AppBar(
+          title: const Text("Hey Sarah, welke leden ga jij \nvandaag helpen?",
+              style: TextStyle(
+                  color: primaryText,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 24)),
+          backgroundColor: primary,
+        ),
+      ),
+      body: const DashboardTickets(),
+    );
   }
 }
