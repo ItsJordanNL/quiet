3503525e5b553f884be622d351b215699c60c226
Koevoets,Jordan J.J.R.A <j.koevoets@student.fontys.nl>
2024-06-13 14:28:17 +0200

    Style - Appbar kleur

--- lib/screens/member/vouchers.dart
+++ lib/screens/member/vouchers.dart
@@ -1,6 +1,6 @@
 import "package:flutter/material.dart";
 import "package:quiet_app/screens/member/components/voucherlist.dart";
-// import "package:quiet_app/screens/member/components/voucherlist.dart";
+import "package:quiet_app/constants/constants.dart";
 
 class VouchersPage extends StatefulWidget {
   const VouchersPage({super.key});
@@ -13,7 +13,11 @@ class _VouchersPageState extends State<VouchersPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Choose a voucher"),),
+      appBar: AppBar(
+        title: const Text("Bekijk de vouchers"),
+        foregroundColor: primaryText, // Set the appbar background color here
+        backgroundColor: primary, // Set the appbar background color here
+      ),
       body: const VoucherList(),
     );
   }
