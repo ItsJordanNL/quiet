24e6e431d284a7892dd8f9b30accca9f91cd6f6b
Koevoets,Jordan J.J.R.A <j.koevoets@student.fontys.nl>
2024-06-13 14:50:40 +0200

    Fix - Geen redirect naar oude chatpagina

--- lib/screens/volunteer/account.dart
+++ lib/screens/volunteer/account.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:quiet_app/constants/constants.dart';
-import 'package:quiet_app/screens/volunteer/components/chat-overview.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
@@ -13,12 +11,7 @@ class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Berichten",
-        style: TextStyle(color: Colors.white)),
-        backgroundColor: primary,
-      ),
-      body: ChatListPage(),
+      appBar: AppBar(title: const Text("Account"),),
     );
   }
 }
