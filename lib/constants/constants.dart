0144b5acd437463f9c0bc1ceaf649b9f4c5b4ef9
Jeanine96 <136748816+Jeanine96@users.noreply.github.com>
2024-06-17 14:31:48 +0200

    [refactor] Added colors for donut chart

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -8,3 +8,8 @@ const Color secondaryText = Color.fromARGB(255, 0, 0, 0);
 const Color primary = Color.fromARGB(255, 133, 168, 39);
 const Color secondary = Color.fromARGB(255, 215, 230, 225);
 const Color backgroundColor = Color.fromARGB(255, 255, 255, 255);
+
+/* ==== Status Colors ====*/
+const Color inprogress = Color.fromARGB(255, 255, 147, 4);
+const Color unanswered = Color.fromARGB(255, 236, 12, 12);
+
