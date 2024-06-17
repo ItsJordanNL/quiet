c85f916d874d376b715f363b8735d1b567f5fbd7
Jeanine96 <136748816+Jeanine96@users.noreply.github.com>
2024-06-17 14:30:51 +0200

    [refactor] Changed ui of screen

--- lib/screens/volunteer/dashboard.dart
+++ lib/screens/volunteer/dashboard.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiet_app/constants/constants.dart';
 import 'package:quiet_app/screens/volunteer/components/dashboard-donutchart.dart';
-
-
+import 'package:quiet_app/screens/volunteer/components/dashboard-questionlist.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
@@ -15,20 +14,20 @@ class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(80),
-        child: AppBar(
-          title: const Text("Hey Sarah, welke leden ga jij \nvandaag helpen?",
-              style: TextStyle(
-                  color: primaryText,
-                  fontWeight: FontWeight.w700,
-                  fontSize: 24)),
-          backgroundColor: primary,
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(80),
+          child: AppBar(
+            title: const Text("Hey Sarah, welke leden ga jij \nvandaag helpen?",
+                style: TextStyle(
+                    color: primaryText,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 24)),
+            backgroundColor: primary,
+          ),
         ),
-      ),
-      body: const SingleChildScrollView(
-        child: DashboardDonutchart(),
-      ),
-    );
+        body: const Stack(
+          children:[DashboardDonutchart(),
+          DashboardQuestionlist() ]),
+        );
   }
 }
