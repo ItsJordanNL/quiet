69823323e70715672ddfbdb03c6e29212b7a06c8
Jeanine96 <136748816+Jeanine96@users.noreply.github.com>
2024-06-21 12:06:18 +0200

    Hotfix Made screen suitable for different screen sizes

--- lib/screens/member/components/voucherinfo.dart
+++ lib/screens/member/components/voucherinfo.dart
@@ -3,9 +3,6 @@ import "package:flutter/material.dart";
 import 'package:quiet_app/screens/member/components/voucherlist.dart';
 import 'package:quiet_app/constants/constants.dart';
 
-// Variable margin bottom
-const space = 15.0;
-
 class VoucherInfo extends StatefulWidget {
   final Voucher voucher;
 
@@ -51,17 +48,21 @@ class _VoucherInfoState extends State<VoucherInfo> {
     final seconds = twoDigits(duration.inSeconds.remainder(60));
     return "$hours:$minutes:$seconds";
   }
+
   // End function timer
 
   @override
   Widget build(BuildContext context) {
+    // Mediaquery for different screensizes
+    final mediaQuery = MediaQuery.of(context);
+    final height = mediaQuery.size.height;
+    final width = mediaQuery.size.width;
+
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(3),
         child: AppBar(
-          title: const Text(
-            "",
-          ),
+          title: const Text(""),
           backgroundColor: primary,
         ),
       ),
@@ -69,19 +70,19 @@ class _VoucherInfoState extends State<VoucherInfo> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Stack(
-            // Image of voucher
             children: [
+              // Picture of voucher
               SizedBox(
                 width: double.infinity,
-                height: 250,
+                height: height * 0.3,
                 child: Image.asset(
                   widget.voucher.imagePath,
                   fit: BoxFit.cover,
                 ),
               ),
               // Back button
-              Container(
-                margin: const EdgeInsets.only(top: 20, left: 0),
+             Container(
+                margin: const EdgeInsets.only(top: 15),
                 child: SizedBox(
                   height: 45,
                   child: ElevatedButton(
@@ -103,168 +104,162 @@ class _VoucherInfoState extends State<VoucherInfo> {
             ],
           ),
           Container(
-            margin: const EdgeInsets.all(20),
+            padding: EdgeInsets.all(width * 0.05),
+            height: height * 0.62,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  margin: const EdgeInsets.only(bottom: space),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      // Category
-                      Text(
-                        widget.voucher.category,
-                        style: const TextStyle(
-                            fontFamily: "Roboto",
-                            fontSize: 17,
-                            fontWeight: FontWeight.w500,
-                            color: primary),
-                      ),
-                      // Timer
-                      Text(
-                        " ${_formatDuration(_timeRemaining)}",
-                        style: const TextStyle(
-                          fontSize: 17,
-                          fontWeight: FontWeight.w700,
-                          color: secondaryText,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                // Title voucher
-                Container(
-                  margin: const EdgeInsets.only(bottom: space),
-                  child: Text(
-                    widget.voucher.titleVoucher,
-                    style: const TextStyle(
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // Category of voucher
+                    Text(
+                      widget.voucher.category,
+                      style: const TextStyle(
                         fontFamily: "Roboto",
-                        fontSize: 24,
+                        fontSize: 17,
                         fontWeight: FontWeight.w500,
-                        color: secondaryText),
+                        color: primary,
+                      ),
+                    ),
+                    // timer
+                    Text(
+                      " ${_formatDuration(_timeRemaining)}",
+                      style: const TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w700,
+                        color: secondaryText,
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(flex: 2,),
+                // Title of voucher
+                Text(
+                  widget.voucher.titleVoucher,
+                  style: const TextStyle(
+                    fontFamily: "Roboto",
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    color: secondaryText,
                   ),
                 ),
+                const Spacer(flex: 2,),
                 // How many persons
-                Container(
-                  margin: const EdgeInsets.only(bottom: 5),
-                  child: Row(
-                    children: [
-                      const Opacity(
-                        opacity: 0.40,
-                        child: Icon(Icons.person_outline_rounded),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(right: 5),
-                      ),
-                      Opacity(
-                        opacity: 0.40,
-                        child: Text(
-                          widget.voucher.persons,
-                          style: const TextStyle(
-                              fontFamily: 'Roboto',
-                              fontSize: 16,
-                              fontWeight: FontWeight.normal),
+                Row(
+                  children: [
+                    const Opacity(
+                      opacity: 0.40,
+                      child: Icon(Icons.person_outline_rounded),
+                    ),
+                    SizedBox(width: width * 0.01),
+                    Opacity(
+                      opacity: 0.40,
+                      child: Text(
+                        widget.voucher.persons,
+                        style: const TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
+                const Spacer(),
                 // Location
-                Container(
-                  margin: const EdgeInsets.only(bottom: space),
-                  child: Row(
-                    children: [
-                      const Opacity(
-                        opacity: 0.40,
-                        child: Icon(Icons.location_on_rounded),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(right: 5),
-                      ),
-                      Opacity(
-                        opacity: 0.40,
-                        child: Text(
-                          widget.voucher.location,
-                          style: const TextStyle(
-                              fontFamily: 'Roboto',
-                              fontSize: 16,
-                              fontWeight: FontWeight.normal),
+                Row(
+                  children: [
+                    const Opacity(
+                      opacity: 0.40,
+                      child: Icon(Icons.location_on_rounded),
+                    ),
+                    SizedBox(width: width * 0.02),
+                    Opacity(
+                      opacity: 0.40,
+                      child: Text(
+                        widget.voucher.location,
+                        style: const TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                // Information about how it works
-                Container(
-                  margin: const EdgeInsets.only(bottom: 10),
-                  child: const Text(
-                    'Activeer deze voucher, voordat de timer op 0 staat.',
-                    style: TextStyle(fontSize: 16, color: secondaryText),
-                  ),
+                const Spacer(flex: 2,),
+                // Info about how it works
+                const Text(
+                  'Activeer deze voucher, voordat de timer op 0 staat.',
+                  style: TextStyle(fontSize: 16, color: secondaryText),
                 ),
-                Container(
-                  margin: const EdgeInsets.only(bottom: 70),
-                  child: const Text(
-                    "Na het activeren van uw voucher, ontvangt u een QR code om toegang te krijgen tot uw geluksmomentje.",
-                    style: TextStyle(fontSize: 16, color: secondaryText),
-                  ),
+              const Spacer(flex: 2),
+                const Text(
+                  "Na het activeren van uw voucher, ontvangt u een QR code om toegang te krijgen tot uw geluksmomentje.",
+                  style: TextStyle(fontSize: 16, color: secondaryText),
                 ),
-                // button activeer
-                Container(
-                  margin: const EdgeInsets.only(bottom: space),
-                  child: SizedBox(
-                      height: 45,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor:
-                                WidgetStateProperty.all<Color>(primary)),
-                        onPressed: () {},
-                        child: const Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "Activeer",
-                                style: TextStyle(
-                                  color: primaryText,
-                                  fontFamily: 'Roboto-Medium',
-                                  fontSize: 17,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                              Icon(
-                                Icons.keyboard_arrow_right_rounded,
-                                color: primaryText,
-                              ),
-                            ]),
-                      )),
-                ),
-
-                // button contact
+                  const Spacer(flex: 8),
+                  // Button activeer
                 SizedBox(
-                    height: 45,
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                          backgroundColor:
-                              WidgetStateProperty.all<Color>(secondaryText)),
-                      onPressed: () {},
-                      child: const Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "Vragen? Verstuur een bericht",
-                              style: TextStyle(
-                                  color: primaryText,
-                                  fontFamily: 'Roboto-Medium',
-                                  fontSize: 17,
-                                  fontWeight: FontWeight.w600),
-                            ),
-                            Icon(
-                              Icons.keyboard_arrow_right_rounded,
-                              color: primaryText,
-                            ),
-                          ]),
-                    )),
+                  height: 45,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: primary,
+                    ),
+                    onPressed: () {},
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Activeer",
+                          style: TextStyle(
+                            color: primaryText,
+                            fontFamily: 'Roboto-Medium',
+                            fontSize: 17,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Icon(
+                          Icons.keyboard_arrow_right_rounded,
+                          color: primaryText,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: height * 0.02),
+                // Button contact
+                SizedBox(
+                  height: 45,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: secondaryText,
+                    ),
+                    onPressed: () {},
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Vragen? Verstuur een bericht",
+                          style: TextStyle(
+                            color: primaryText,
+                            fontFamily: 'Roboto-Medium',
+                            fontSize: 17,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Icon(
+                          Icons.keyboard_arrow_right_rounded,
+                          color: primaryText,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
@@ -273,3 +268,4 @@ class _VoucherInfoState extends State<VoucherInfo> {
     );
   }
 }
+
